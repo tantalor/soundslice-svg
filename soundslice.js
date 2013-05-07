0ee8b019868a8c04886a83426862c45491f168d7
John Tantalo <john.tantalo@gmail.com>
2013-05-07 00:02:58 -0700

    added Head class

--- soundslice.js
+++ soundslice.js
@@ -1,52 +1,60 @@
 var paper = Raphael('demo');
 
-function createHead() {
-  var set = paper.set();
+function Head() {
+  this.set = paper.set();
   var diamond = paper.path("M10,0L20,10L10,20L0,10L10,0");
   diamond.attr('stroke', 'rgb(163,71,24)');
   diamond.attr('fill', 'rgb(232,129,61)');
   diamond.attr('cursor', 'pointer');
-  set.push(diamond);
+  this.set.push(diamond);
+  this.diamond = diamond;
 
   var line = paper.path("M10,20L10,600");
   line.attr('stroke', 'rgb(232,129,61)');
   line.attr('stroke-width', 2);
-  set.push(line);
+  this.set.push(line);
+  this.line = line;
   
-  var glow = set.glow({
+  var glow = this.set.glow({
     swidth: 8,
     fill: true,
     color: 'rgb(232,129,61)'});
   glow.hide();
-  set.push(glow);
+  this.set.push(glow);
+  this.glow = glow;
   
   // Drag events.
-  var x0 = 10, y0 = 10, dx0 = 0, xmin = 10;
-  set.drag(onmove, onstart)
-  function onmove(dx, dy, x, y) {
-    dx0 = dx;
-    update();
-  };
-  function onstart() {
-    x0 += dx0;
-    dx0 = 0;
-  };
-  function update() {
-    var x = Math.max(x0 + dx0, xmin);
-    set.transform('T' + x + ',' + y0);
-  };
+  this.x = 10;
+  this.y0 = 10;
+  this.dx = 0;
+  this.xmin = 10;
+  this.set.drag(this.onmove.bind(this), this.onstart.bind(this));
   
   // Hover events.
-  diamond.hover(hoverin, hoverout);
-  function hoverin() {
-    glow.show();
-  };
-  function hoverout() {
-    glow.hide();
-  };
+  diamond.hover(this.hoverin.bind(this), this.hoverout.bind(this));
   
-  update();
-  return set;
+  this.update();
 };
 
-var head = createHead();
+Head.prototype = {
+  onmove: function(dx, dy, x, y) {
+    this.dx = dx;
+    this.update();
+  },
+  onstart: function() {
+    this.x += this.dx;
+    this.dx = 0;
+  },
+  update: function() {
+    var x = Math.max(this.x + this.dx, this.xmin);
+    this.set.transform('T' + x + ',' + this.y0);
+  },
+  hoverin: function() {
+    this.glow.show();
+  },
+  hoverout: function() {
+    this.glow.hide();
+  }
+};
+
+var head = new Head();
